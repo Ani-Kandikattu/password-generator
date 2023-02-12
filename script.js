ef376ee3d4431a695c37eb78e6857f54ed0b1fbf
Anirudh-Kandikattu <anikandikattu@gmail.com>
2023-02-12 14:58:30 -0700

    moved style.css into assets folder and wrote pseudocode in comments

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//created the function generatePassword
+function generatePassword() {
+  // 1. Prompt the user for the password length between 8 - 128, lowercase and uppercase:
+
+  // 2. Validate input:
+
+  // 3. Generate the password:
+
+  // 4. Display the password to the page:
+
+  return "Password: ";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
