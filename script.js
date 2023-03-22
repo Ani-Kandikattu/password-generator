b1cb8a38fc507c344d06c1397b90c1c591ac1d18
Ani-Kandikattu <120747994+Ani-Kandikattu@users.noreply.github.com>
2023-03-22 13:16:15 -0700

    code for generating password

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
+var passwordInput = document.getElementById("password");
 
-//created the function generatePassword
-function generatePassword() {
-  // 1. Prompt the user for the password length between 8 - 128, lowercase and uppercase:
+//Click event so that when generate button is clicked, user is prompted
+generateBtn.addEventListener("click", function () {
+  var passwordLength = parseInt(
+    prompt("Enter the desired length of the password (8-128):"),
+    10
+  );
 
-  // 2. Validate input:
+  //Confirmation for including uppercase, lowercase, numbers and special characters
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    let password = "";
+    var includeUppercase = confirm("Include uppercase characters?");
+    var includeLowercase = confirm("Include lowercase characters?");
+    var includeNumbers = confirm("Include numbers?");
+    var includeSpecial = confirm("Include special characters?");
 
-  // 3. Generate the password:
+    //The range of characters to choose from
+    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "0123456789";
+    var specialChars = "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
 
-  // 4. Display the password to the page:
+    var charSet = "";
+    if (includeUppercase) charSet += uppercaseChars;
+    if (includeLowercase) charSet += lowercaseChars;
+    if (includeNumbers) charSet += numbers;
+    if (includeSpecial) charSet += specialChars;
 
-  return "Password: ";
-}
+    //generating the random password password
+    for (var i = 0; i < passwordLength; i++) {
+      password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    //If length chosen is not between 8 and 128
+    passwordInput.value = password;
+  } else {
+    alert("Password length must be between 8 and 128 characters.");
+  }
+});
